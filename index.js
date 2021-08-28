7331766633138dff693851e6b1ae8eafa686148a
NatalyFarias <nataly.fariasg@gmail.com>
2021-08-28 19:47:13 -0400

    se agrega funcion leer directorio y imprimir archivo en formato string. ademas se configura package.json

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+const pathModule = require('path');
+const EXTENSION_MD = '.md';
+
+const readFolder = (location) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(location, (err, files) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(files)
+      }
+    })
+  })
 };
+
+const printFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        const dataAsString = data.toString();
+        resolve(dataAsString)
+      }
+    })
+  })
+};
+
+const mdLinks = (path) => {
+  readFolder(path).then(files => {
+    files.forEach(file => {
+      if (pathModule.extname(file) === EXTENSION_MD) {
+        printFile(file).then(archivo => {
+          console.log(archivo);
+        }).catch(err => console.log(err))
+      }
+    })
+  }).catch(err => console.log(err))
+}
+
+const fileName = process.argv[2];
+
+module.exports.mdLinks = mdLinks(fileName)

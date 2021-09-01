374d995f7f1c4d6b10ee3edbc6b8fad0c1edd90a
NatalyFarias <nataly.fariasg@gmail.com>
2021-09-01 00:01:07 -0400

    se agrega fincionalidad de tomar links e imprimirlos en consola

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const fs = require('fs');
 const pathModule = require('path');
 const EXTENSION_MD = '.md';
+const regularExpresionUrl = /\]\((http[^)]+)\)/g
+
 //creo funcion para luego en la funcion principal pueda ser llamada
 //esta funcion verifica si es un directorio
 const isDirectory = (path) => {
   //usamos el modulo fs e invoco el metodo lstatSync
-  const value = fs.lstatSync(parameterType).isDirectory();
+  const value = fs.lstatSync(path).isDirectory();
   return value;
 }
 //esta funcion se encarga de leer directorios
@@ -21,39 +23,43 @@ const readFolder = (location) => {
     })
   })
 };
-//esta funcion se encarga de leer un archivo e imprimir su contenido en la terminal
-const printFile = (filename) => {
+//esta funcion se encarga de leer un archivo y tomar links
+const takeLinks = (filename) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, (err, data) => {
       if (err) {
         reject(err)
       } else {
+        let cleanLinks = []
         const dataAsString = data.toString();
-        resolve(dataAsString)
+        const allLinks = dataAsString.match(regularExpresionUrl)
+        allLinks.forEach(e => {
+          cleanLinks.push(e.replace(/[\[\(\)\]]/g, ''))
+        })
+        resolve(cleanLinks)
       }
     })
   })
 };
-//funcion principal md link verifica si los enlaces estan buenos
+
+//funcion principal md link que lee los archivos 
 const mdLinks = (path) => {
-if(isDirectory(path)){
-  readFolder(path).then(files => {
-    files.forEach(file => {
-      if (pathModule.extname(file) === EXTENSION_MD) {
-        printFile(file).then(archivo => {
-          console.log(archivo);
-        }).catch(err => console.log(err))
-      }
-    })
-  }).catch(err => console.log(err))
+  if (isDirectory(path)) {
+    readFolder(path).then(files => {
+      files.forEach(file => {
+        if (pathModule.extname(file) === EXTENSION_MD) {
+          takeLinks(file).then(links => {
+            console.log(links);
+          }).catch(err => console.log(err))
+        }
+      })
+    }).catch(err => console.log(err))
+  }else{
+    // Aqui ira la funcionalidad cuando se envie un archivo
+  }
 }
-}
- 
+
 // PASO 1 : Lectura de parametro process.argv[2]
 // Declarando variable parameterType y asignamos lo que envia la terminal
 const parameterType = process.argv[2];
-//usamos el modulo fs e invoco el metodo lstatSync
-const valor = fs.lstatSync(parameterType).isDirectory();
-console.log('Es un directorio : ' + valor);
-
 module.exports.mdLinks = mdLinks(parameterType)
